2febc80f6f36ccd1c128117c0ac508f112e7fcc6
EshaalYasir123 <eshaalyasir87@gmail.com>
2025-11-12 00:22:52 +0500

    login and signup files

--- client/COLLIDE-FRONTEND/src/app/auth/auth/login/login.ts
+++ client/COLLIDE-FRONTEND/src/app/auth/auth/login/login.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.scss'
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html', 
+  styleUrls: ['./login.scss']
 })
 export class Login {
+  email: string = '';
+  password: string = '';
+  selectedTab: string = 'login'; 
 
+
+  setTab(tab: string) {
+    this.selectedTab = tab;
+  }
+
+  onLogin() {
+    console.log('Logging in...');
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+   
 }
--- client/COLLIDE-FRONTEND/src/app/auth/auth/signup/signup.ts
+++ client/COLLIDE-FRONTEND/src/app/auth/auth/signup/signup.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss'
 })
 export class Signup {
+  email: string = '';
+  password: string = '';
+  reEnterPassword: String = ''
+  selectedTab: string = 'login';
+
+  setTab(tab: string) {
+    this.selectedTab = tab;
+  }
+  onSignUp(){
+        console.log('Signing up...');
+        console.log('Email:', this.email);
+        console.log('Password:', this.password);
+        console.log('reEnterPassword', this.reEnterPassword)
+      }
+ 
 
 }
